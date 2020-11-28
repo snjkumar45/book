546c0d6187fbefba7ecb64a30911dfcbeaf2f7c4
sanjay <snjkumar45@outlook.com>
2020-11-28 17:14:42 +0530

    error occure in this session.skip this session

---
+++ src/main/java/com/book/book/controller/FileUploadController.java
@@ -0,0 +1,19 @@
+package com.book.book.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileUploadController {
+    @GetMapping("/upload)")
+    public ResponseEntity<String> uplaodFile(@RequestParam("file")MultipartFile file){
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getSize());
+        System.out.println(file.getContentType());
+        System.out.println(file.getName());
+        return  ResponseEntity.ok("okok") ;   }
+    
+} 
